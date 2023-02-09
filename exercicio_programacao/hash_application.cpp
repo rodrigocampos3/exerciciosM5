eb0e4a1faf193dad53556574fed1c8d96ed6dff1
rodrigorodriguesb <92267398+rodrigorodriguesb@users.noreply.github.com>
2023-02-09 17:07:32 -0300

    update

--- exercicio_programacao/hash_application.cpp
+++ exercicio_programacao/hash_application.cpp
@@ -2,6 +2,7 @@
 #include "hash.h" 
 
 using namespace std;
+// Esta função trás um exemplo de implemetação de tabela hash sem colisões,onde cada imei e usuario estão sendo inseridos em seus lugares corretos na lista
 void caso1(){
   Hash hashClient(10);
    int   imeis[7]   = {
@@ -21,6 +22,7 @@ void caso1(){
   hashClient.print();  
   }
 
+// Esta função trás um exemplo de implemetação onde temos colisões, porém elas estão sendo tradada. Quando a posição que o item deveria ser inserido estiver ocupada, o algoritivo insere o item na posição seguinte
 void caso2(){
     Hash hashClient(10);
    int   imeis[7]   = {
@@ -38,6 +40,8 @@ void caso2(){
   }
   hashClient.print();
 }
+
+// o algoritimo abaixo mostra um exemplo de busca de item em uma lista onde não ocorrem colsões
 void caso3(){
     Hash hashClient(10);
    int   imeis[7]   = {
@@ -59,6 +63,8 @@ void caso3(){
   hashClient.retrieveItem(client, found);
   cout << client.getNome() << " -> " << found << endl;
   }
+
+// o código abaixo mostra um exemplo de busca onde existem colisões
 void caso4(){
     Hash hashClient(10);
    int   imeis[7]   = {
@@ -81,10 +87,12 @@ void caso4(){
   hashClient.retrieveItem(client, found);
   cout << client.getNome() << " -> " << found << endl;
 }
+
+// Abaixo temos um exemplo de exclusão em uma tabela onde não temos colisões
 void caso5(){
       Hash hashClient(10);
    int   imeis[7]   = {
-     12704, 12704, 1234,
+     12704, 12705, 1234,
      49001, 52202, 65606,
      91234};
   
